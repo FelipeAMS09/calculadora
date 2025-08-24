dfe04ad8ee87393bfe1354599e3c99cf94869484
Felipe Meira <felipe.ameira@hotmail.com>
2025-08-24 19:57:51 -0300

    melhora-funcionalidade

--- funcoes.py
+++ funcoes.py
@@ -10,20 +10,46 @@ def soma(x):
         elif condition == '+':
             print(result)
             number2 = int(input('Digite outro número: '))
-            result = result + number2    
+            result = result + number2
+        elif condition == '-':
+            print(result)
+            number2 = int(input('Digite outro número: '))
+            result = result - number2
+        elif condition == '*':
+            print(result)
+            number2 = int(input('Digite outro número: '))
+            result = result * number2  
+        elif condition == '/':
+            print(result)
+            number2 = int(input('Digite outro número: '))
+            result = result / number2   
     print(result)
 
-def subtracao(x,y):
-    x = int(input('Digite um número: '))
-    y = int(input('Digite outro número: '))
-    resultado = x - y
-    return resultado
+def subtracao(x):
+    number2 = int(input('Digite outro número: '))
+    result = x - number2
+    while True:
+        condition = input('Escolha a operação: (+) (-) (*) (/) (=):')
+        if condition == '=':
+            break
+        elif condition == '-':
+            print(result)
+            number2 = int(input('Digite outro número: '))
+            result = result - number2    
+    print(result)
 
-def multiplicacao(x,y):
-    x = int(input('Digite um número: '))
-    y = int(input('Digite outro número: '))
-    resultado = x * y
-    return resultado
+def multiplicacao(x):
+    number2 = int(input('Digite outro número: '))
+    result = x * number2
+    while True:
+        condition = input('Escolha a operação: (+) (-) (*) (/) (=):')
+        if condition == '=':
+            break
+        elif condition == '*':
+            print(result)
+            number2 = int(input('Digite outro número: '))
+            result = result * number2    
+    print(result)
 
 def divisao(x,y):
     x = int(input('Digite um número: '))
--- main.py
+++ main.py
@@ -1,10 +1,104 @@
-import funcoes
 
 
 print('---CALCULADORA---')
-number = int(input('Digite um número: '))
-choiceOperation = input('Escolha a operação: (+) (-) (*) (/): ')
 
+while True:
+    try:
+        number = int(input('Digite um número: '))   
+    except ValueError:
+        print('Escolha um NÚMERO!!!')
+        continue
+    while True:
+        try:
+            choiceOperation = input('Escolha a operação: (+) (-) (*) (/): ')
+            if choiceOperation not in '+-/*':
+                print('Escolha um dos simbolos!!')
+            else:
+                break
+        except ValueError:
+            print('Escolha um dos Simbolos!!')
+            continue
+    break
 
-if choiceOperation == '+':
-    funcoes.soma(number)
+while True:
+
+    if choiceOperation == '+':
+        try:
+            number2 = int(input('Digite outro número: ')) 
+        except ValueError:
+            print('Escolha um NÚMERO!!')
+            continue
+        result = number + number2
+
+    elif choiceOperation == '-':
+        try:
+            number2 = int(input('Digite outro número: '))
+        except ValueError:
+            print('Escolha um NÚMERO!!')
+            continue
+        result = number - number2
+
+    elif choiceOperation == '*':
+        try:
+            number2 = int(input('Digite outro número: '))
+        except ValueError:
+            print('Escolha um NÚMERO!!')
+            continue
+        result = number * number2    
+
+    elif choiceOperation == '/':
+        try:
+            number2 = int(input('Digite outro número: '))
+        except ValueError:
+            print('Escolha um NÚMERO!!')
+            continue
+        result = number / number2  
+
+    while True:
+        condition = input('Escolha a operação: (+) (-) (*) (/) (=):')
+        if condition == '=':
+            break
+        elif condition == '+':
+            print(result)
+            while True:
+                try:
+                    number2 = int(input('Digite outro número: '))
+                except ValueError:
+                    print('Escolha um NÚMERO!!')
+                    continue
+                result = result + number2
+                break
+
+        elif condition == '-':
+            print(result)
+            while True:
+                try:
+                    number2 = int(input('Digite outro número: '))
+                except ValueError:
+                    print('Escolha um NÚMERO!!')
+                    continue
+                result = result - number2
+                break
+
+        elif condition == '*':
+            print(result)
+            while True:
+                try:
+                    number2 = int(input('Digite outro número: '))
+                except ValueError:
+                    print('Escolha um NÚMERO!!')
+                    continue
+                result = result * number2 
+                break
+
+        elif condition == '/':
+            print(result)
+            while True:
+                try:
+                    number2 = int(input('Digite outro número: '))
+                except ValueError:
+                    print('Escolha um NÚMERO!!')
+                    continue
+                result = result / number2
+                break
+    print(result)
